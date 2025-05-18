2656aca9a8c6cd33fc4a065cf049ea7e2c4cbdec
dev <sempernow.com@gmail.com>
2025-05-18 14:01:27 -0400

    README.md @ 2025-05-18T18:00Z

---
+++ README.md
@@ -0,0 +1,16 @@
+[k8s-iperf](https://github.com/sempernow/k8s-iperf)
+
+
+>Bandwidth (Throughput) test of your K8s Cluster (Pod) Network.
+
+- Both same-node and cross-node cases are measured.
+- Often referred to as east-west or inter-pod traffic,
+  these throughput measurements are expected to vary
+  by host network, CNI provider, their configurations,
+  and adjacent network-traffic conditions.
+
+## Usage
+
+```bash
+bash k8s-iperf.sh
+```
---
+++ k8s-iperf.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+#################################################################
+# Bandwidth (Throughput) test of your K8s Cluster (Pod) Network.
+# 
+# - Both same-node and cross-node cases are measured.
+# - Often referred to as east-west or inter-pod traffic, 
+#   these throughput measurements are expected to vary 
+#   by host network, CNI provider, their configurations, 
+#   and adjacent network-traffic conditions.
+#
+# ARGs: [PORT(else 5555)]
+#################################################################
+clear;echo '
+=======================================================
+🚀  Bandwidth test of K8s Pod Network using iperf3
+'
+
+img=nicolaka/netshoot # https://github.com/nicolaka/netshoot
+port=${1:-5555} # Presumed okay
+
+echo -e '\n🚧 === Create and set context to a per-run Namespace …'
+nonce="$(cat /dev/urandom |tr -dc 'a-z0-9' |fold -w 7 |head -n 1)" || 
+    nonce="$(date '+%H.%M.%S%z')"
+ns="test-iperf3-$nonce"
+kubectl create ns $ns
+kubectl get ns $ns || {
+    echo "⚠ === ERR : Namespace '$ns' NOT EXIST"
+    exit $?
+}
+kubectl config set-context --current --namespace $ns
+
+echo -e "\n🚧 === Traffic port: $port"
+
+echo -e '\n🚧 === Creating the server …'
+
+# Server
+sPod=server
+kubectl run $sPod --image=$img -- iperf3 -s -p $port
+while [[ -z $sNode || -z $sIP ]]; do
+    echo "Awaiting Node and IP status of Pod '$sPod' …" 
+    export sNode=$(kubectl get pod $sPod -o jsonpath='{.spec.nodeName}')
+    export sIP=$(kubectl get pod $sPod -o wide -o jsonpath='{.status.podIPs[].ip}')
+    sleep 2
+done
+
+echo "✅ === Server pod '$sPod' running at node '$sNode'." 
+
+# Clients : One case at a time
+cPod=client
+cNode=$sNode
+echo "Next, run client pods '$cPod' sequentially (IntRA-node, IntER-node) …"
+
+# - Same-node (IntRA-node) case
+echo -e "\n📊 === Same-node ($sNode-$cNode) traffic between server '$sPod@$sNode' and client '$cPod@$cNode' [Pod@Node] …"
+kubectl run $cPod -it --rm \
+    --image=$img \
+    --overrides='{"spec": {"nodeName": "'$sNode'"}}' \
+    --restart=Never  -- \
+    iperf3 -c $sIP -p $port
+while kubectl get pod $cPod &> /dev/null; do
+    echo "Awaiting deletion of Pod '$cPod' …"
+    sleep 2
+done
+
+# - Cross-node (IntER-node) case
+cNode=$(kubectl get node -oname -o yaml |yq '.[][].metadata |select (.name != "'$sNode'") |.name' |head -n1)
+echo -e "\n📊 === Cross-node ($sNode-$cNode) traffic between server '$sPod@$sNode' and client '$cPod@$cNode' [Pod@Node] …"
+kubectl run $cPod -it --rm \
+    --image=$img \
+    --overrides='{"spec": {"nodeName": "'$cNode'"}}' \
+    --restart=Never  -- \
+    iperf3 -c $sIP -p $port
+# Teardown
+echo -e '\n🚧 === Teardown'
+kubectl config set-context --current --namespace default
+kubectl delete ns ${ns:-___nonexistent_namespace___}
+echo -e '\n======================[ DONE ]=========================\n'
